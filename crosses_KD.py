b1505fcb2da912fbfe174c64134898c0afed5eac
samirak93 <samirakumar@yahoo.com>
2018-01-21 10:17:49 +0530

    Update crosses_KD.py

--- crosses_KD.py
+++ crosses_KD.py
@@ -1,16 +1,15 @@
-%matplotlib inline
+## Coding created by Samira Kumar 2018.
 
 import numpy as np
-from sklearn.cluster import MeanShift
+import matplotlib
 import matplotlib.pyplot as plt
+matplotlib.use('TkAgg')
 import pandas as pd
 import pylab as pl
-import seaborn
-from sklearn.cluster import KMeans
-from sklearn import neighbors
-import scipy.spatial as spatial
 
-df = pd.read_csv('/Users/samirakumar/Desktop/Samir/Crosses/crosses_updated.csv')
+import scipy.spatial as spatial
+color_map = plt.cm.copper
+df = pd.read_csv('insert location of crosses data file')
 headers = ["cross_id", "x", "y","pass_end_x", "pass_end_y"]
 crosses = pd.DataFrame(df, columns=headers)
 
@@ -18,45 +17,55 @@ points = np.array(crosses[['x','y']])
 point_tree = spatial.cKDTree(points)
 fig = plt.figure()
 
-pitch = pl.imread('/Users/samirakumar/Desktop/Samir/Passes/base.png')
+
+pitch = pl.imread('insert location of football pitch')
 ax = fig.add_subplot(1,1,1)
 ax.imshow(pitch, zorder=0, aspect=None,extent=[-50,105,0,105])
 
 ax.text(-48, 2, '@SamiraK93', fontsize=10)
 pl.gca().axison = False
 
+#On click event to plot dynamic graph based on user mouse clicks.
 
 def onclick(event):
     plt.clf()
     global ix, iy
+
     ix, iy = event.xdata, event.ydata
+
     global x_test
     ax = fig.add_subplot(1, 1, 1)
     x_test=np.array([ix,iy])
-    #x_test1 = np.array([[x_test[0], x_test[1]]])
 
+    #Plots the user click as a blue circle
     ax.scatter(event.xdata, event.ydata, s=40,c='b')
+    
+    #Plots the range of nearest neighbour search as a outer circle with red color
     circle1 = plt.Circle((event.xdata, event.ydata), 2, color='r')
-    c=plt.Circle((event.xdata, event.ydata),radius=3,color='g',fill=False,linewidth=2)
+    c=plt.Circle((event.xdata, event.ydata),radius=6,color='g',fill=False,linewidth=2)
+    #radius is defined from search range from below line. I.e- 6
+    
     ax.add_patch(c)
     fig.canvas.draw()
 
     pl.gca().axison = False
-    ax.scatter(crosses.pass_end_x[(point_tree.query_ball_point([ix, iy], 3))],
-             crosses.pass_end_y[(point_tree.query_ball_point([ix, iy], 3))], s=20,c='red')
-    #ax.hexbin(crosses.pass_end_x[(point_tree.query_ball_point([ix, iy], 6))],
-     #          crosses.pass_end_y[(point_tree.query_ball_point([ix, iy], 6))], gridsize=20,cmap='inferno',extent='scalar')
-
-    #plt.scatter(x_test[0], x_test[1], s=40,c='b')
+    plt.hexbin(crosses.pass_end_x[(point_tree.query_ball_point([ix, iy], 6))],
+              crosses.pass_end_y[(point_tree.query_ball_point([ix, iy], 6))],extent=[-50, 105, 0, 105],linewidths=0.5,edgecolors='white', gridsize=40,cmap=color_map,mincnt=2)
+    
+    # 6 represents finding points within 6 unit of cross start location
+    
     ax = fig.add_subplot(111)
     ax.text(-48, 2, '@SamiraK93', fontsize=10)
+
     ax.imshow(pitch, zorder=0,aspect=None, extent=[-50, 105, 0, 105])
     pl.gca().axison = False
+    fig.canvas.manager.window.activateWindow()
 
     return x_test
 
 
 
 cid = fig.canvas.mpl_connect('button_press_event', onclick)
+#plotly.offline.iplot(fig)
 
 plt.show()
